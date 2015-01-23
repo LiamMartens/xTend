3c917d2c0d75fe1e176bdca45a01587a5fa95e4a
LiamMartens <Liam9512@outlook.com>
2015-01-23 14:08:52 +0100

    Backup update

--- dist/System/Core/Backup.php
+++ dist/System/Core/Backup.php
@@ -18,14 +18,18 @@
 					$BackupName = $Files[count($Files)-1];
 				}
 				//open archive and extract backup
-				$bak = new Archive(Dir::System("Backups.Temp"), true);
-				$bak->Extract();
-				//restore all files
-
+				$bak = new Archive(File::System("Backups.$BackupName"), true);
+				$bak->Extract(Dir::System("Backups.Temp"));
+				//restore all files --> loop
+				$TempFiles = Dir::RecursiveFiles(Dir::System("Backups.Temp"));
+				foreach($TempFiles as $File) {
+					$FileContent = File::Read(Dir::System("Backups.Temp").$File);
+					file_put_contents("../".$File, $FileContent);
+				}
 				//remove temp directory
 				Dir::Remove(Dir::System("Backups.Temp"));
 				//Remove backup zip file
-				File::Remove(File::System("Backups.Temp.".$BackupName));
+				File::Remove(File::System("Backups.".$BackupName));
 			}
 		}
 	}
