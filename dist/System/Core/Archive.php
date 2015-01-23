8f27002e6a0371f79cc7925519b947d3ee6c66ff
LiamMartens <Liam9512@outlook.com>
2015-01-23 09:21:54 +0100

    changing backup methods

--- dist/System/Core/Archive.php
+++ dist/System/Core/Archive.php
@@ -4,16 +4,15 @@
 		class Archive
 		{
 			private $Zip;
-			private $Source;
 			private $Destination;
 			
-			public function __construct($Dest) {
+			public function __construct($Dest, $Read = false) {
 				//set source and destination
-				$this->Source = $Src;
 				$this->Destination = $Dest;
 				//start zip archive
 				$this->Zip = new \ZipArchive;
-				$this->Zip->open($this->Destination, \ZipArchive::CREATE);
+				//open ziparchive as read or write
+				if($Read) { $this->Zip->open($this->Destination); } else { $this->Zip->open($this->Destination, \ZipArchive::CREATE); }
 			}
 			
 			public function AddFile($FilePath, $ZipPath) {
@@ -30,9 +29,13 @@
 					$this->Zip->addFile($DirPath."/".$File,$File);
 				}
 			}
+
+			public function Extract($Dest) {
+				return $this->Zip->extractTo($Dest);
+			}
 			
 			public function Save() {
-				$this->Zip->close();
+				return $this->Zip->close();
 			}
 		}
 	}
--- dist/System/Core/Backup.php
+++ dist/System/Core/Backup.php
@@ -10,8 +10,22 @@
 				$bak->Save();
 			}
 			
-			public static function Restore() {
-				
+			public static function Restore($Name = false) {
+				$BackupName = $Name.".zip";
+				//restore last backup or provided name
+				if($Name==false) {
+					$Files = Dir::Files(Dir::System("Backups"));
+					$BackupName = $Files[count($Files)-1];
+				}
+				//open archive and extract backup
+				$bak = new Archive(Dir::System("Backups.Temp"), true);
+				$bak->Extract();
+				//restore all files
+
+				//remove temp directory
+				Dir::Remove(Dir::System("Backups.Temp"));
+				//Remove backup zip file
+				File::Remove(File::System("Backups.Temp.".$BackupName));
 			}
 		}
 	}
