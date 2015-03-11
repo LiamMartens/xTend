988671f0c8d23e172bd4505efc6fdf357a8e33a3
LiamMartens <Liam9512@outlook.com>
2015-03-11 17:07:12 +0100

    Small Model Initialization Update

--- dist/System/Core/Models.php
+++ dist/System/Core/Models.php
@@ -7,14 +7,16 @@
 				return File::Exists(File::System("Models.$Model.php"));
 			}
 			//Initialize a model
-			public static function Initialize($Model) {
+			public static function Initialize($Model, $CreateInstance = true) {
 				//Check whether model exists
 				if(self::Exists($Model)) {
 					//Include model
 					App::IncludeFile("Models.$Model.php");
 					$ModelName = explode('.', $Model);
-					$ModelName = $ModelName[count($ModelName) - 1];
-					App::Model(new $ModelName());
+					if($CreateInstance==true) {
+						$ModelName = $ModelName[count($ModelName) - 1];
+						App::Model(new $ModelName());
+					}
 					return true;
 				}
 				return false;
