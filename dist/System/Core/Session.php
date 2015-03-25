7d436b4755446a434d6bd7710e7c07e1cbd77d66
LiamMartens <Liam9512@outlook.com>
2015-03-25 10:02:43 +0100

    Better session manager

--- dist/System/Core/Session.php
+++ dist/System/Core/Session.php
@@ -3,6 +3,20 @@
 	{
 		class Session
 		{
+			public static function Name() {
+				return session_name();
+			}
+
+			public static function Destroy() {
+				if(isset($_COOKIE["sname"])) {
+					session_destroy($_COOKIE["sname"]);
+					setcookie("sname", null, time()-1, '/');
+					unset($_COOKIE["sname"]);
+					return true;
+				}
+				return false;
+			}
+
 			public static function Start() {
 				$sname = session_name();
 				if(!isset($_COOKIE["sname"])){setcookie('sname',sha1($_SERVER['REMOTE_ADDR']).hash("sha512", microtime()),time()+3600*24,'/');}
