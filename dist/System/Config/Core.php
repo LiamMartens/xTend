ee3cf06d64b3bd7570c4641885250c4ebeb1e9db
LiamMartens <Liam9512@outlook.com>
2015-05-10 00:54:22 +0200

    Version change

--- dist/System/Config/Core.php
+++ dist/System/Config/Core.php
@@ -3,7 +3,7 @@
 	{
 		class Config
 		{
-			const xTendVersion = "0.5.9";
+			const xTendVersion = "0.6.0";
 			const Url = "http://104.236.51.149";
 			const Development = false;
 			const Company = "";
