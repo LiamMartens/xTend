6c73c13aee9e5d996cd1f5d6c94d8d093226301b
Liam Martens <hi@liammartens.com>
2015-07-10 23:05:41 +0200

    0.17 Leafo SCSS PHP

--- dist/System/Config/Sass.php
+++ dist/System/Config/Sass.php
@@ -1,17 +1,13 @@
 <?php
+	use Leafo\ScssPhp\Compiler;
 	if(xTend\Config::Development==true) {
+		//all scss file names you want to compile
 		$sass_files = array("main");
-		// scssc will be loaded automatically via Composer
-		$scss_compiler = new scssc();
-		// set the path where your _mixins are
-		$scss_compiler->setImportPaths("sass/");
-		// set css formatting (normal, nested or minimized), @see http://leafo.net/scssphp/docs/#output_formatting
-		//$scss_compiler->setFormatter($format_style);
+		$scss = new Compiler();
+		$scss->setImportPaths("sass/");
 		foreach($sass_files as $file) {
 			$string_sass = file_get_contents("sass/$file.scss");
-			// compile this SASS code to CSS
-			$string_css = $scss_compiler->compile($string_sass);
-			// write CSS into file with the same filename, but .css extension
+			$string_css = $scss->compile($string_sass);
 			file_put_contents("css/$file.css", $string_css);
 		}
 	}
