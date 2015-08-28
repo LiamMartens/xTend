feb95825b2490bf659a40b4b058af489a938e62b
LiamMartens <hi@liammartens.com>
2015-08-28 18:48:50 +0200

    Stupid fix

--- dist/System/Config/Variables.php
+++ dist/System/Config/Variables.php
@@ -2,6 +2,6 @@
 	namespace xTend
 	{
 		Variables::Set('app.root', substr(__DIR__, 0, strlen(__DIR__) - 14));
-		Variables::Set('app.web', Variables::Get('app.root').'\public');
+		Variables::Set('app.web', Variables::Get('app.root').'\www');
 		Variables::Set('app.system', Variables::Get('app.root').'\System');
 	}
