145099e43c2d54aaea76f2ed76ee4d9e9afb0ad4
Liam Martens <Liam9512@outlook.com>
2015-12-18 10:44:07 +0100

    Small docs fic

--- dist/www/index.php
+++ dist/www/index.php
@@ -3,6 +3,6 @@
 	{
 		require_once("..\\System\\Core\\App.php");
 		//this allows for multiple apps
-		$app=xTend\createNewApp(__NAMESPACE, __DIR__);
+		$app=\xTend\createNewApp(__NAMESPACE, __DIR__);
 		$app->run();
 	}
