c64c136d7f368a2cf153322bb9eedc5971cfff6e
Liam Martens <Liam9512@outlook.com>
2015-12-18 10:43:29 +0100

    Index fix

--- dist/www/index.php
+++ dist/www/index.php
@@ -1,5 +1,8 @@
 <?php
-	require_once("..\\System\\Core\\App.php");
-	//this allows for multiple apps
-	$app=xTend\createNewApp(__DIR__);
-	$app->run();
+	namespace xTend\Application
+	{
+		require_once("..\\System\\Core\\App.php");
+		//this allows for multiple apps
+		$app=xTend\createNewApp(__NAMESPACE, __DIR__);
+		$app->run();
+	}
