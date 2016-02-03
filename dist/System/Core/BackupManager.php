fd07cdbd128d1ad7f77531222dcc78637fb644c6
Liam Martens <Liam9512@outlook.com>
2016-02-03 19:04:41 +0100

    Directory config integration in backupsmanager

--- dist/System/Core/BackupManager.php
+++ dist/System/Core/BackupManager.php
@@ -15,7 +15,7 @@
 			private function needsBackup() {
 				if($this->_app->getBackupInterval()!==false) {
 					$interval = strtotime($this->_app->getBackupInterval());
-					$backups = $this->_app->getDirectoryHandler()->files($this->_app->getDirectoryHandler()->systemDirectory("Backups")); sort($backups);
+					$backups = $this->_app->getDirectoryHandler()->files($this->_app->getDirectoryHandler()->systemDirectory($this->_app->getBackupsDirectory())); sort($backups);
 					if(count($backups)>0) {
 						$last_backup = $backups[count($backups) - 1];
 						$last_backup_name = substr($last_backup, 0, strrpos($last_backup, "."));
@@ -27,7 +27,7 @@
 			}
 			private function cleanBackups() {
 				if($this->_app->getBackupLimit()!==false) {
-					$backups = $this->_app->getDirectoryHandler()->files($this->_app->getDirectoryHandler()->systemDirectory("Backups"), true); sort($backups);
+					$backups = $this->_app->getDirectoryHandler()->files($this->_app->getDirectoryHandler()->systemDirectory($this->_app->getBackupsDirectory()), true); sort($backups);
 					$to_remove = count($backups) - $this->_app->getBackupLimit();
 					if($to_remove>0) {
 						for($i=0;$i<$to_remove;$i++) {
@@ -38,13 +38,14 @@
 			}
 			public function create($force=false) {
 				if((!$this->needsBackup())&&(!$force)) return false;
-				$bak = new Archive($this->_app->getFileHandler()->systemFile("Backups.".time()."-".date("YmdHis").".zip"));
+				$bak = new Archive($this->_app->getFileHandler()->systemFile($this->_app->getBackupsDirectory().".".time()."-".date("YmdHis").".zip"));
 				$systemfiles = $this->_app->getDirectoryHandler()->recursiveFiles($this->_app->getSystemDirectory());
 				$sysdir_len = strlen($this->_app->getSystemDirectory())+1;
 				$sysdir_name = substr($this->_app->getSystemDirectory(), strrpos($this->_app->getSystemDirectory(), "/")+1);
+				$bak_dir_pref = $this->_app->getDirectoryHandler()->systemDirectory($this->_app->getBackupsDirectory());
 				foreach ($systemfiles as $sysfile) {
 					$sysfile_relname = substr($sysfile, $sysdir_len);
-					if(substr($sysfile_relname, 0, 8)!="Backups/") { $bak->addFile($sysfile, "$sysdir_name/$sysfile_relname"); }
+					if(substr($sysfile_relname, 0, 8)!="$bak_dir_pref/") { $bak->addFile($sysfile, "$sysdir_name/$sysfile_relname"); }
 				}
 				$publicfiles = $this->_app->getDirectoryHandler()->recursiveFiles($this->_app->getPublicDirectory());
 				$pubdir_len = strlen($this->_app->getPublicDirectory())+1;
